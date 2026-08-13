12ac8a4a18d7a688bf17a94eee3a2dd883a3e4a6
daniel <danielfgayoso@gmail.com>
2026-08-13 15:18:06 +0200

    line parsing

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-# define LSH_BUFFER_SIZE 1024
+#define LSH_BUFFER_SIZE 1024
+#define LSH_TOKEN_DELIMITERS " \t\r\n\a"
+#define LSH_TOKEN_SIZE 64
 
 char *lsh_read_line(void){
 	// Creo un buffer donde almacenar el input del usuario, por defecto 1024 caracteres.
@@ -22,9 +25,9 @@ char *lsh_read_line(void){
 	// 	- En caso de cualquier caracter distinto a \n o EOF, lo inserta en la posición indicada y la incrementa para la próxima iteración.
 	while(1){
 		c = getchar();
-
+		
 		if(c == '\n' || c == EOF){
-			buffer = '\0';
+			buffer[position] = '\0';
 			printf("EOF detected...\n");
 			return buffer;
 		}
@@ -34,15 +37,43 @@ char *lsh_read_line(void){
 
 		if(position >= buffer_size){
 			buffer_size += LSH_BUFFER_SIZE;
-			realloc(buffer, buffer_size);
+			buffer = realloc(buffer, buffer_size);
 		}
 
 		if(!buffer){
 			printf("Error in memory reallocation\n");
-			free(buffer);
 			exit(EXIT_FAILURE); 
 		}
 	}	
+}
+
+char **lsh_split_line(char *line){
+	size_t buffer_size = LSH_TOKEN_SIZE;
+	char **tokens =  malloc(buffer_size * sizeof(char*));
+	char *token;
+	int position = 0;
+
+	if(!tokens){
+		printf("Error in memory allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	token = strtok(line, LSH_TOKEN_DELIMITERS);
+	while(1){
+		if(token == NULL){
+			return tokens;	
+		}
+
+		tokens[position] = token;
+		position++;
+
+		if(position >= buffer_size){
+			buffer_size+=LSH_TOKEN_SIZE;
+			tokens = realloc(tokens, buffer_size*sizeof(char*));
+		}
+
+		token = strtok(NULL, LSH_TOKEN_DELIMITERS);
+	}
 }
 
 void lsh_loop(void){
@@ -52,11 +83,12 @@ void lsh_loop(void){
 
 	while(status){
 		printf("-> ");
-		line = lsh_read_line();
 		printf("Reading line..\n");
-		// args = lsh_split_line(line);
-		// printf("Reading args...")
-		// status = lsh_execute_line();
+		line = lsh_read_line();
+		printf("Parsing line...\n");
+		//args = lsh_split_line(line);
+		//printf("Reading args...");
+		//status = lsh_execute_line();
 	}
 	
 	free(args);
